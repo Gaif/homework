c7e5c3b78168dd11feee4f67fc2cdb19016d379d
gaizunfu <gaizunfu@163.com>
2022-02-26 20:39:08 +0800

    'initwork'

---
+++ w1-1/contracts/Counter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public counter;
+
+    constructor() {
+        counter = 0;
+    }
+
+    function count() public {
+        counter++;
+    }
+
+    function set(uint x) public {
+        counter = counter + x;
+    }
+    
+
+}
